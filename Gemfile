4d420e8888b86a86eda3ded7052d13b05345d99f
Steve Klabnik <steve@steveklabnik.com>
2013-04-23 12:43:41 +0200

    FRP version.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+ruby '1.9.3', :engine => 'jruby', :engine_version => '1.7.3'
+
+gem "shoes4", github: "shoes/shoes4"
+gem "frappuccino", github: "steveklabnik/frappuccino"
---
+++ frp_shoes.rb
@@ -0,0 +1,51 @@
+require 'frappuccino'
+
+class Button
+  def initialize(event)
+    @event = event
+  end
+
+  def push
+    emit(@event)
+  end
+end
+
+Shoes.app :height => 150, :width => 250 do
+  background rgb(240, 250, 208)
+
+  @plus = Button.new(:+)
+  @minus = Button.new(:-)
+
+  @stream_one = Frappuccino::Stream.new(@plus)
+  @stream_two = Frappuccino::Stream.new(@minus)
+
+  @merged_stream = Frappuccino::Stream.merge(@stream_one, @stream_two)
+  @counter = @merged_stream
+             .map do |event|
+                case event
+                when :+
+                  1
+                when :-
+                  -1
+                else
+                  0
+                end
+              end
+            .inject(0) {|sum, n| sum + n }
+
+  stack :margin => 10 do
+    button "+1" do
+      @plus.push
+    end
+
+    button "-1" do
+      @minus.push
+    end
+
+    @label = para "Current count: #{@counter.to_i}"
+  end
+
+  every do
+    @label.replace "Current count: #{@counter.to_i}"
+  end
+end
